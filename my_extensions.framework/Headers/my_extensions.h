f984095a6e0004ead4589ca817982175b32f71eb
durodolahabib043 <durodolahabib043@gmail.com>
2018-11-01 16:28:22 -0400

    first commit

---
+++ my_extensions.framework/Headers/my_extensions.h
@@ -0,0 +1,19 @@
+//
+//  my_extensions.h
+//  my-extensions
+//
+//  Created by Habib Durodola on 10/1/18.
+//  Copyright © 2018 Habib Durodola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for my_extensions.
+FOUNDATION_EXPORT double my_extensionsVersionNumber;
+
+//! Project version string for my_extensions.
+FOUNDATION_EXPORT const unsigned char my_extensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <my_extensions/PublicHeader.h>
+
+
